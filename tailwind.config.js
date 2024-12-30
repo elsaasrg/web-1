5616c7ab703ba4a20fcce4eef4e9aa1237baa8a3
elsaasrg <elsadaimareta17@gmail.com>
2024-12-30 11:42:20 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        customblue: 'rgb(73,73,126)'
+      },
+      fontFamily:{
+        poppins:['Poppins', 'serif'],
+      }
+
+    },
+  },
+  plugins: [],
+}
+
